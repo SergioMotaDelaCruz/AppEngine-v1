3410d785e9d6744849732456706d66e863ce14e3
SergioMotaDelaCruz <sergio.mota.delacruz@gmail.com>
2018-03-26 11:24:27 +0200

    V1

---
+++ appengine_config.py
@@ -0,0 +1,4 @@
+from google.appengine.ext import vendor
+
+# Add libraries installed in the "libs" folder to the application.
+vendor.add('libs')
---
+++ start.py
@@ -0,0 +1,36 @@
+import logging
+
+from flask import Flask, render_template, request
+from google.appengine.ext import ndb
+from google.appengine.api import memcache
+
+
+application = Flask(__name__)
+
+class Version(ndb.Model):
+   likes=ndb.FloatProperty()
+   views=ndb.FloatProperty()
+
+v1=Version.get_by_id('v1')
+if not v1:
+   v1=Version(likes=1,views=1,id='v1')
+   v1_key=v1.put()
+
+@application.route('/')
+def home():
+   v1_last=Version.get_by_id('v1')
+   v1_last.views+=1
+   v1_last.put()
+   return render_template('home.html')
+
+@application.route('/sumav1', methods=['POST'])
+def sumav1():
+   v1_last=Version.get_by_id('v1')
+   v1_last.likes+=1
+   v1_last.put()
+   return render_template('like-v1.html')
+
+@application.errorhandler(500)
+def server_error(e):
+    logging.exception('Error during request. '+str(e))
+    return 'An internal error occurred.', 500
